aace5a95ca27220bafeff51c704453bb196c398a
Sushan Poudel <sushanpoudel80@gmail.com>
2025-05-23 10:55:15 +0545

    feat(schema): update users schema

--- web/database/migrations/0001_01_01_000000_create_users_table.php
+++ web/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,13 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role', ['patient', 'doctor', 'admin', 'superadmin'])->default('patient');
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->enum('gender', ['male', 'female', 'other']);
+            $table->date('dob')->nullable();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
