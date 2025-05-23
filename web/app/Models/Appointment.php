a99e48d5bde018cfc24cf205ce6b468b5220b1d3
Sushan Poudel <sushanpoudel80@gmail.com>
2025-05-23 11:08:09 +0545

    feat(model):update specialization model

--- web/app/Models/Appointment.php
+++ web/app/Models/Appointment.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    //
+        protected $fillable = [
+        'patient_id',
+        'doctor_id',
+        'appointment_date',
+        'slot',
+        'status',
+    ];
+    public function doctor(){
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function patient(){
+        return $this->belongsTo(Patient::class);
+    }
+
+    // public function payment(){
+    //     return $this->hasOne(Payment::class);
+    // }
+
+    // public function message(){
+    //     return $this->hasOne(Message::class);
+    // }
 }
